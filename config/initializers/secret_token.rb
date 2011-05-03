fef5931d0cbe25a4ee2f2f20cd9da40049117a8a
allansideas <allansideas@gmail.com>
2011-05-03 13:27:30 +1200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heike::Application.config.secret_token = 'ba8143b545340abd314d4760237fef00fa526889a751e28c704bb5e5bcc07bc9315a2486c132092fa59d0e308ffed1d13927f5115dee72ed22b0ece42289dcc9'
